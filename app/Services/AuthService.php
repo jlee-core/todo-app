2497315c615c4fda94be96238f5c7869010f36b8
j.lee <j.lee@core-tech.jp>
2026-05-07 13:22:25 +0900

    user作成際にapi_tokenを自動生成するように修正

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class AuthService {
     public function createUser(array $data): User
@@ -15,6 +16,7 @@ class AuthService {
                 'name' => $data['name'],
                 'email' => $data['email'],
                 'password' => Hash::make($data['password']),
+                'api_token' => Str::random(80),
             ]);
 
             return $user;
