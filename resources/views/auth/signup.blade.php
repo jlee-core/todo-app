d01db0c5afa9b02f3071af2fca4a02eba97c8b6c
j.lee <j.lee@core-tech.jp>
2026-05-07 14:11:20 +0900

    csrfを追加

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -5,22 +5,23 @@
 @section('content')
 <h2>新規作成</h2>
 <form method="POST" action=" {{ route('signup.store') }} ">
+    @csrf
     <div>
-    <label for="name">ニックネーム</label>
-    <input id="name" type="text" name="name">
-</div>
+        <label for="name">ニックネーム</label>
+        <input id="name" type="text" name="name">
+    </div>
 
-<div>
-    <label for="email">メールアドレス</label>
-    <input id="email" type="email" name="email">
-</div>
+    <div>
+        <label for="email">メールアドレス</label>
+        <input id="email" type="email" name="email">
+    </div>
 
-<div>
-    <label for="password">パスワード</label>
-    <input id="password" type="password" name="password">
-</div>
+    <div>
+        <label for="password">パスワード</label>
+        <input id="password" type="password" name="password">
+    </div>
 
-<button type="submit">作成</button>
+    <button type="submit">作成</button>
 </form>
 
 <form method="GET" action="{{ route('login') }}">
